21cb7517f5bc7c38f1e9444b0d0ecee475b79ba9
Gadir Calderón Díaz <99917841+gadyrcdz@users.noreply.github.com>
2026-04-21 15:35:47 -0600

    Feat: To do functions

--- cps_backtracking/csp/csp.py
+++ cps_backtracking/csp/csp.py
@@ -1,25 +1,37 @@
-# class Course
+import copy
+
+
+class Course:
 #     function __init__(name, domain)
 #         name <- name
 #         domain <- domain
 #         value <- empty
+    def __init__(self, name, domain):
+        self.name = name
+        self.domain = domain
+        self.value;
 
 #     function assign(value)
 #         value of the course <- value
-
+    def assign(self,value):
+        self.value = value
 #     function remove assignment
 #         value of the course <- empty
-
+    def remove_assignment(self):
+        self.value = 0
 
 # function initialize(variables, domain)
 #     courses <- empty list
-
 #     for each variable in variables
 #         course <- new course with that variable and a copy of the domain
 #         add course to courses
-
 #     return courses
-
+    def initialize(self, variables, domain):
+        courses = []
+        for each in variables:
+            course = Course(each, copy.deepcopy(domain))
+            courses.append(course)
+        return courses
 
 # function is consistent(course, assigned courses, constraints)
 #     assigned by name <- dictionary of assigned courses using the name as key
@@ -41,6 +53,28 @@
 
 #     return true
 
+    def is_consistent(self, course, assignedCourses, constraints):
+        assigned_by_name = assignedCourses.get("name", 0)
+
+        for each in constraints:
+            left, right = each.split("!=")
+
+            if(course is not left and course is not right):
+                continue
+            
+            if(course == left):
+                other_name = right
+            else:
+                other_name = left
+
+            other_course = assignedCourses.get(other_name, 0)
+            if(other_course == 0):
+                continue
+            if(course.value == other_course):
+                return False
+            
+        return True
+                
 
 # function backtracking(course, remaining courses, assigned courses, constraints)
 #     for each day in the domain of the course
